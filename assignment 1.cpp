68a66fed615e3ca2b624aad55ea6bd58226a9429
Aryan Mishra <aryanmishra5879@gmail.com>
2024-06-18 12:19:20 +0530

    Create assignment 1.cpp

---
+++ assignment 1.cpp
@@ -0,0 +1,90 @@
+// Ques: How can you output “Physics”and  “Wallah” in two different lines in C++?
+
+// Answer
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Physics"<<endl;
+    cout<<"Wallah";
+}
+
+
+
+
+
+// Ques: Print 10 using 2 positive numbers less than 6  in C++ as output.
+// Answer
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 4;
+    int num2 = 6;
+    int result = num1 + num2;
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
+
+
+// Ques: How much space does following data types take ?
+// Answer===>
+
+// int = 4;
+// bool = 1;
+// float = 4;
+
+
+
+// Ques: What is the output of this program?
+
+// void main(){
+// 	int a=4;
+// int b=5;
+// a++;b--;
+// cout<<++a<<” “<<b–-;
+// }
+
+
+// Answer
+// == >6 4
+
+
+
+
+// Ques: WAP to find the circumference of a circle with radius 10 in C++.
+// Answer
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float radius = 10.0;      
+    float pi = 3.14159;      
+    float circumference  = 2 * pi * radius;;    
+  
+
+    // Output the result
+    cout << "The circumference of the circle with radius " << radius << " is " << circumference << endl;
+
+    return 0;  
+}
+
+
+
+
+
+// Ques: How many of these can be a variable name?
+// 01PWskills
+//  _FLOAT
+// int
+// _FLOAT
+// You will succeed 
+
+// Answer ==>
+
+//   these 2 will be a variable name _FLOAT ; FLOAT;
+
